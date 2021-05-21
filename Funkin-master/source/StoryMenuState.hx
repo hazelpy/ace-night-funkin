ba6006002068f8f287751680e277f86509d23dfa
hazelpy <59073660+hazelpy@users.noreply.github.com>
2021-05-21 19:14:03 -0400

    adasdasd

--- Funkin-master/source/StoryMenuState.hx
+++ Funkin-master/source/StoryMenuState.hx
@@ -173,7 +173,7 @@ class StoryMenuState extends MusicBeatState
 				case 'ace-phase-one':
 					weekCharacterThing.setGraphicSize(Std.int(weekCharacterThing.width * 0.9));
 					weekCharacterThing.y -= 100;
-					weekCharacterThing.x -= 50;
+					weekCharacterThing.x -= 250;
 			}
 
 			grpWeekCharacters.add(weekCharacterThing);
@@ -429,8 +429,8 @@ class StoryMenuState extends MusicBeatState
 				grpWeekCharacters.members[0].setGraphicSize(Std.int(grpWeekCharacters.members[0].width * 1));
 
 			case 'ace-phase-one':
-				grpWeekCharacters.members[0].offset.set(-100, 200);
-
+				grpWeekCharacters.members[0].offset.set(0, 200);
+				
 			default:
 				grpWeekCharacters.members[0].offset.set(100, 100);
 				grpWeekCharacters.members[0].setGraphicSize(Std.int(grpWeekCharacters.members[0].width * 1));
