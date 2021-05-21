e86288fc7fb7750cf2eb38970a4d3ed40da4e6bb
hazelpy <59073660+hazelpy@users.noreply.github.com>
2021-05-21 19:02:15 -0400

    asd

--- Funkin-master/source/StoryMenuState.hx
+++ Funkin-master/source/StoryMenuState.hx
@@ -427,8 +427,6 @@ class StoryMenuState extends MusicBeatState
 			case 'dad':
 				grpWeekCharacters.members[0].offset.set(120, 200);
 				grpWeekCharacters.members[0].setGraphicSize(Std.int(grpWeekCharacters.members[0].width * 1));
-			case 'ace-phase-one':
-				grpWeekCharacters.members[0].offset.set(150, 200);
 			default:
 				grpWeekCharacters.members[0].offset.set(100, 100);
 				grpWeekCharacters.members[0].setGraphicSize(Std.int(grpWeekCharacters.members[0].width * 1));
