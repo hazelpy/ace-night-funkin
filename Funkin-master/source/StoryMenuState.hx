12890294c2f73521a4b020d89653a4dea860c334
hazelpy <59073660+hazelpy@users.noreply.github.com>
2021-05-21 19:17:26 -0400

    hail mary

--- Funkin-master/source/StoryMenuState.hx
+++ Funkin-master/source/StoryMenuState.hx
@@ -173,7 +173,7 @@ class StoryMenuState extends MusicBeatState
 				case 'ace-phase-one':
 					weekCharacterThing.setGraphicSize(Std.int(weekCharacterThing.width * 0.9));
 					weekCharacterThing.y -= 100;
-					weekCharacterThing.x -= 1050;
+					weekCharacterThing.x += 100;
 			}
 
 			grpWeekCharacters.add(weekCharacterThing);
