1b24862175fe04858e1f0d333eeb23c30b756a8b
hazelpy <59073660+hazelpy@users.noreply.github.com>
2021-05-21 18:59:31 -0400

    slow down idiot

--- Funkin-master/source/StoryMenuState.hx
+++ Funkin-master/source/StoryMenuState.hx
@@ -428,7 +428,7 @@ class StoryMenuState extends MusicBeatState
 				grpWeekCharacters.members[0].offset.set(120, 200);
 				grpWeekCharacters.members[0].setGraphicSize(Std.int(grpWeekCharacters.members[0].width * 1));
 			case 'ace-phase-one':
-				grpWeekCharacters.members[0].offset.set(150, 250);
+				grpWeekCharacters.members[0].offset.set(150, 200);
 			default:
 				grpWeekCharacters.members[0].offset.set(100, 100);
 				grpWeekCharacters.members[0].setGraphicSize(Std.int(grpWeekCharacters.members[0].width * 1));
