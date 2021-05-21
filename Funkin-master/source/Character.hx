34d419a3653c43ce1eed626ef387fb38063a6ed5
hazelpy <59073660+hazelpy@users.noreply.github.com>
2021-05-21 17:45:40 -0400

    offset~

--- Funkin-master/source/Character.hx
+++ Funkin-master/source/Character.hx
@@ -149,6 +149,12 @@ class Character extends FlxSprite
 				animation.addByPrefix('singLEFT', 'ace_phase_one_left', 24);
 				animation.addByPrefix('singUP', 'ace_phase_one_up', 24);
 
+				addOffset('idle', 0, -10);
+				addOffset("singUP", 0, -10);
+				addOffset("singRIGHT", 0, -10);
+				addOffset("singLEFT", 0, -10);
+				addOffset("singDOWN", 0, -10);
+
 				playAnim('idle');
 			case 'spooky':
 				tex = Paths.getSparrowAtlas('spooky_kids_assets');
