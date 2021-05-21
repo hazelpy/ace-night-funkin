be1873aebee6af06e54d046a9fb530b7a3d781fa
hazelpy <59073660+hazelpy@users.noreply.github.com>
2021-05-21 18:37:09 -0400

    tutoril

--- Funkin-master/source/MenuCharacter.hx
+++ Funkin-master/source/MenuCharacter.hx
@@ -25,7 +25,8 @@ class MenuCharacter extends FlxSprite
 		animation.addByPrefix('mom', "Mom Idle BLACK LINES", 24);
 		animation.addByPrefix('parents-christmas', "Parent Christmas Idle", 24);
 		animation.addByPrefix('senpai', "SENPAI idle Black Lines", 24);
-		animation.addByPrefix('ace-phase-one', "ace-phase-one", 24);
+		animation.addByPrefix('ace-phase-one', "ace-phase-one", 12);
+		addOffset('ace-phase-one', -50, 200);
 		// Parent Christmas Idle
 
 		animation.play(character);
